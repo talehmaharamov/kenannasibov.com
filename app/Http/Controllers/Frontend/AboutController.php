32aae5bf5ac6ac3fff07709775bcbd854236e515
Taleh Maharramov <talehmeherrem85@gmail.com>
2023-01-03 20:05:17 +0400

    View

--- app/Http/Controllers/Frontend/AboutController.php
+++ app/Http/Controllers/Frontend/AboutController.php
@@ -12,6 +12,7 @@ class AboutController extends Controller
     public function index(){
         $about = About::find(1);
         $directors = Director::where('status', 1)->get();
+
         $forigners = Forigner::all();
         return view('frontend.about.index',get_defined_vars());
     }
